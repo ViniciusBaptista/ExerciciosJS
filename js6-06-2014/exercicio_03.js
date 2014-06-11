b74c5f56bb2509ad81f1258767bc3428b8875d79
vbaptista <vbaptista7@gmail.com>
2014-06-11 15:00:50 -0300

    Alguns exercicios feitos

---
+++ js6-06-2014/exercicio_03.js
@@ -0,0 +1,13 @@
+﻿//Escreva uma função isVowel(vowel) que retorne true se a letra
+//informada for uma vogal.
+
+function isVowel(vowel) {
+  var vogais = ["a", "e", "i", "o", "u"];
+  for(var i = 0; i < vogais.length; i++) {
+  	if(vowel === vogais[i]){
+  	  return true;
+  	}
+  }
+  return false;
+}
+isVowel();
---
+++ js6-06-2014/exercicio_11.js
@@ -0,0 +1,7 @@
+//Escreva uma função scramble(text) que retorna um texto bagunçado
+//randomicamente
+function scramble(text){
+	var text1 = text.split(" ");
+	text1.reverse();
+	return text1;
+}
